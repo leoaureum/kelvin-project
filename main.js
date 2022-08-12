72065e4bcec404b2d4a91814d6d11c7dc3ab1ac6
leoaureum <108500523+leoaureum@users.noreply.github.com>
2022-08-12 10:09:43 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const kelvin = 50
+
+// const keeps the variable 'kelvin' the same throughout the code. 'kelvin' cannot be assigned to any othe string
+
+celsius = kelvin - 273;
+
+
+// using the subtraction function, I defined 'celsius' as 'kelvin' - 273 in accordance with the conversation rate
+
+// Fahrenheit = celsius * (9/5) + 32;
+
+// used the equation to convert celsius to fahrenheit, made sure () were in the right place
+
+Fahrenheit = Math.floor((celsius * (9/5)) + 32);
+
+newton = Math.floor(celsius * (33/100));
+
+var CTEMPERATURE = Fahrenheit
+
+var NTEMPERATURE = newton
+
+console.log(`The temperature is ${CTEMPERATURE} degrees Fahrenheit and ${NTEMPERATURE} degrees Newton`);
